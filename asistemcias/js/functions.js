6f917ad994bc24d7b5e32f1b4275fb2ecf76f073
Jorge Figueroa <yknx.4.b@gmail.com>
2016-03-03 10:43:24 -0600

    lalal

--- asistemcias/js/functions.js
+++ asistemcias/js/functions.js
@@ -4,101 +4,120 @@ function getToken(){
 
 
 function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+  sURLVariables = sPageURL.split('&'),
+  sParameterName,
+  i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
     }
+  }
 }
 
 function setPaymentDataToSales(sales_data, payment_data) {
-    sales_data.forEach(function (sale) {
-        sale.remaining = sale.price;
-        for (var i = 0; i < payment_data.length; i++) {
-            var thisPayment = payment_data[i];
-            if (thisPayment.sale_id == sale._id) {
-                sale.remaining -= thisPayment.price;
-            }
-        }
-    });
+  sales_data.forEach(function (sale) {
+    sale.remaining = sale.price;
+    for (var i = 0; i < payment_data.length; i++) {
+      var thisPayment = payment_data[i];
+      if (thisPayment.sale_id == sale._id) {
+        sale.remaining -= thisPayment.price;
+      }
+    }
+  });
 }
 
 
+function Attendance(student_id, club_id, partial){
+  this.student_id = student_id;
+  this.club_id = club_id;
+  this.user_id = "55ff0ff4ac872d2442b57067";
+  this.partial =  partial;
+  this.date = Date.now();
+}
+
+function Student(account, campus, name){
+  this.name = name;
+  this.account = account;
+  this.school = campus;
+  this.email = 0;
+  this.modified = Date.now();
+  this.created = Date.now();
+}
+
 function getProductData(product_id, products_data) {
-    for (var i = 0; i < products_data.length; i++) {
-        var thisProduct = products_data[i];
-        if (thisProduct._id == product_id) {
-            return thisProduct.name + " - $" + (Math.round(thisProduct.price * 100) / 100);
-        }
+  for (var i = 0; i < products_data.length; i++) {
+    var thisProduct = products_data[i];
+    if (thisProduct._id == product_id) {
+      return thisProduct.name + " - $" + (Math.round(thisProduct.price * 100) / 100);
     }
-    return "";
+  }
+  return "";
 }
 
 
 function setProductsDataToSales(sales_data, products_data) {
-    sales_data.forEach(function (sale) {
-        sale.remaining = sale.price;
-        sale._products = [];
-        for (var i = 0; i < sale.products.length; i++) {
-            var thisProductId = sale.products[i];
-            var thisProduct = getProductData(thisProductId, products_data);
-            sale._products.push(thisProduct);
-        }
-    });
+  sales_data.forEach(function (sale) {
+    sale.remaining = sale.price;
+    sale._products = [];
+    for (var i = 0; i < sale.products.length; i++) {
+      var thisProductId = sale.products[i];
+      var thisProduct = getProductData(thisProductId, products_data);
+      sale._products.push(thisProduct);
+    }
+  });
 }
 
 function getTotalDebt(sales_data) {
-    var total = 0;
-    for (var i = 0; i < sales_data.length; i++) {
-        var thisSale = sales_data[i];
-        total += thisSale.remaining;
-    }
-    return total;
+  var total = 0;
+  for (var i = 0; i < sales_data.length; i++) {
+    var thisSale = sales_data[i];
+    total += thisSale.remaining;
+  }
+  return total;
 }
 
 
+
+
 function generateSaleHtml(sale) {
-    var date = new Date(Number(sale.date));
-    var html_1 = '<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><h3>Sale: '
-    var date_txt = date.toDateString();
-    var html_2 = '</h3></div><div class="panel-body"><ul class="list-group">';
-    var product_data = "";
-    for (var i = 0; i < sale._products.length; i++) {
-        product_data += '<li class="list-group-item">';
-        product_data += sale._products[i];
-        product_data += '</li>';
-    }
-    var html_3 = '</ul></div><div class="panel-footer text-right">Remaining: $'
-    var remaining = Math.round(sale.remaining * 100) / 100;
-    var html_4 = '<br><small>Total: $'
-    var total = Math.round(sale.price * 100) / 100;
-    var html_5 = '</small></div></div></div>';
-    return html_1 + date_txt + html_2 + product_data + html_3 + remaining + html_4 + total + html_5;
+  var date = new Date(Number(sale.date));
+  var html_1 = '<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><h3>Sale: '
+  var date_txt = date.toDateString();
+  var html_2 = '</h3></div><div class="panel-body"><ul class="list-group">';
+  var product_data = "";
+  for (var i = 0; i < sale._products.length; i++) {
+    product_data += '<li class="list-group-item">';
+    product_data += sale._products[i];
+    product_data += '</li>';
+  }
+  var html_3 = '</ul></div><div class="panel-footer text-right">Remaining: $'
+  var remaining = Math.round(sale.remaining * 100) / 100;
+  var html_4 = '<br><small>Total: $'
+  var total = Math.round(sale.price * 100) / 100;
+  var html_5 = '</small></div></div></div>';
+  return html_1 + date_txt + html_2 + product_data + html_3 + remaining + html_4 + total + html_5;
 }
 
 
 /*
 <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3>Sale: November 3rd, 2016</h3></div>
-                <div class="panel-body">
-                    <ul class="list-group">
-                       Pulparindo - $15.00</li>
-                        <li class="list-group-item">USB - $13.4</li>
-                        <li class="list-group-item">Dulce - $7.00</li>
-                    </ul>
-                </div>
-                <div class="panel-footer text-right">Remaining: $34.00
-                    <br><small>Total: $156.03</small></div>
-            </div>
-        </div>
+<div class="panel panel-default">
+<div class="panel-heading">
+<h3>Sale: November 3rd, 2016</h3></div>
+<div class="panel-body">
+<ul class="list-group">
+Pulparindo - $15.00</li>
+<li class="list-group-item">USB - $13.4</li>
+<li class="list-group-item">Dulce - $7.00</li>
+</ul>
+</div>
+<div class="panel-footer text-right">Remaining: $34.00
+<br><small>Total: $156.03</small></div>
+</div>
+</div>
 
 */
--- asistemcias/js/main.js
+++ asistemcias/js/main.js
@@ -1,71 +1,183 @@
- var client_id = "561afb52588d69d463d5dce8";
- var sales_path = "http://sales-yknx4.rhcloud.com/sale?client_id=";
- var client_path = "http://sales-yknx4.rhcloud.com/client?";
- var payment_path = "http://sales-yknx4.rhcloud.com/payment?";
- var product_path = "http://sales-yknx4.rhcloud.com/product?";
- var sales_data;
- var client_data;
- var payment_data;
- var product_data;
+var clubs_path = 'http://attendance-yknx4.rhcloud.com/club';
+var attendance_path = 'http://attendance-yknx4.rhcloud.com/attendance';
+var students_path = 'http://attendance-yknx4.rhcloud.com/student';
+var clubs_data;
+var selected_club = null;
+var selected_partial = 0;
+var students_data = null;
 
- var clubs_path = 'http://attendance-yknx4.rhcloud.com/club';
- var clubs_data;
 
- function loadClubs(data){
-   console.log('Loading clubs');
-   clubs_data = data;
-   console.log(data);
-   console.log('Populating club options');
-   var options = $("#club_select");
-   $.each(data, function() {
-     options.append($("<option />").val(this._id).text(this.name));
-   });
- }
+//Containers
+var partials = $("#partial_select");
 
- function loadClient(data) {
-     console.log("Loading client")
-     data.forEach(function (client) {
-         if (client._id == client_id) {
-             client_data = client;
-         }
-     })
-     $.get(payment_path, loadPayments, "json");
- }
+//Text Fields
+var txtAccount = $("#account");
+var txtAccountReg = $("#accountReg");
+var txtCampus = $("#campusReg");
+var txtName = $("#nameReg");
 
- function loadProducts(data) {
-     console.log("Loading products")
-     product_data = data;
-     setProductsDataToSales(sales_data, product_data);
-     $.get(client_path, loadClient, "json");
- }
+//Buttons
+var btn_add = $("#btnAdd");
+var btn_reg = $("#btnRegister");
 
- function loadSales(data) {
-     console.log("Loading sales")
-     sales_data = data;
-     $.get(product_path, loadProducts, "json");
- }
+//ERROR FIELDS
+var errorAccount = $("#errorAccount");
+var errorRegister = $("#errorRegister");
 
- function loadPayments(data) {
-     console.log("Loading payments")
-     payment_data = data;
-     setPaymentDataToSales(sales_data, payment_data);
-     start();
- }
 
- function start() {
-     console.log(client_data);
-     console.log(sales_data);
-     console.log(payment_data);
-     console.log(product_data);
-     console.log("Generating page...")
-     $(".first-name").text(client_data.name);
-     $(".total-debt").text(getTotalDebt(sales_data));
-     sales_data.forEach(function (sale) {
-         var html = generateSaleHtml(sale);
-         $(".sale-container").append(html);
-     });
- }
+function getStudentId(accountNumber, callback){
+  for(var i = 0; i < students_data.length; i++){
+    if(students_data[i].account === accountNumber){
+      return students_data[i]._id;
+    }
+  }
+  return 0;
+}
 
- $(document).ready(function () {
-     $.get(clubs_path, loadClubs, "json");
- });
+function loadStudents(data){
+  console.log('Loading students');
+  students_data = data;
+}
+
+function loadClubs(data){
+  console.log('Loading clubs');
+  clubs_data = data;
+  console.log(data);
+  console.log('Populating club options');
+  var options = $("#club_select");
+
+
+  $.each(data, function() {
+    options.append($("<option />").val(this._id).text(this.name).data('partials',this.partials));
+  });
+
+
+  options.change(function(){
+    var selected = $("#club_select option:selected");
+    console.log('Club selected: '+selected.val());
+    selected_club = selected.val();
+    selected_partial = 0;
+    $(".main_title").text(selected.text());
+    console.log('Adding partials: '+selected.data('partials'))
+    partials.empty().append($("<option />").val(0).text('Select a partial...'));
+    for(var i=1; i<=selected.data('partials');i++){
+      partials.append($("<option />").val(i).text(i));
+    }
+
+  });
+}
+
+function register(){
+  var pathWithToken = students_path +'?'+ $.param({token: getToken()});
+  var accountNumber = parseInt(txtAccountReg.val());
+  var name = txtName.val();
+  var campus = txtCampus.val()
+  if(accountNumber === 0){
+    console.log('Error, account invalid');
+    errorAccount.text('Account is invalid');
+    return;
+  }
+
+  var studentId = getStudentId(accountNumber);
+  if(studentId !== 0){
+    console.log('Error, account alreadi exists');
+    errorRegister.text('Acccount already exists');
+    return;
+  }
+
+  if(name === '' || campus === '' ){
+    console.log('Error, invalid data');
+    errorRegister.text('Fill all the data!');
+    return;
+  }
+  var student= new Student(accountNumber,campus,name);
+  $.ajax({
+    url: pathWithToken,
+    type: 'post',
+    data: JSON.stringify(student),
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    dataType: 'json',
+    success: onRegister,
+    error: onRegisterFail
+  });
+}
+
+function addAttendance(){
+  var pathWithToken = attendance_path +'?'+ $.param({token: getToken()});
+  var accountNumber = parseInt(txtAccount.val());
+  var studentId = getStudentId(accountNumber);
+  if(studentId === 0){
+    console.log('Error, account not found');
+    errorAccount.text('Invalid Account');
+    return;
+  }
+
+  var attendace = new Attendance(studentId,selected_club,selected_partial);
+  $.ajax({
+    url: pathWithToken,
+    type: 'post',
+    data: JSON.stringify(attendace),
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    dataType: 'json',
+    success: onAttendance,
+    error: onAttendanceFail
+  });
+}
+
+function onAttendance(data){
+  console.log('Attendance response');
+  console.log(data);
+}
+
+function onAttendanceFail(data){
+  console.log('Attendance failure');
+  console.log(data);
+}
+
+function onRegister(data){
+  console.log('Register response');
+  console.log(data);
+  students_data.push(data);
+  txtAccountReg.val('');
+  txtName.val('');
+}
+
+function onRegisterFail(data){
+  console.log('Attendance failure');
+  console.log(data);
+  errorRegister.text('Something went wrong.');
+}
+
+function btnAddOnClick(){
+  console.log('Adding attendance');
+  if(selected_partial===0 || selected_club === null){
+    alert('You haven\'t selected all data');
+    return;
+  }
+  errorAccount.text('');
+  addAttendance();
+}
+
+function btnRegisterOnClick(){
+  console.log('Registering');
+  errorRegister.text('');
+  register();
+}
+
+$(document).ready(function () {
+  $.get(clubs_path, loadClubs, "json");
+  $.get(students_path, loadStudents, "json");
+  partials.change(function(){
+    var selected = $("#partial_select option:selected");
+    console.log('Partial selected: '+selected.val());
+    selected_partial = selected.val();
+  });
+
+  btn_add.click(btnAddOnClick);
+  btn_reg.click(btnRegisterOnClick);
+
+});
